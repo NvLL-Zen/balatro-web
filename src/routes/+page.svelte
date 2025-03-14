106141edfd524006aa49239221f2812d70a04adb
Zenrieru <nvll.zen@gmail.com>
2025-03-14 09:09:18 +0700

    V2 changed borders to white

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,25 @@
 </script>
 <div class="mainScreen">
         <div class="menu">
-            <div class="roundDetails">? Bind</div>
+            <div class="roundDetails">
+                <h1 class="blindSize">
+                    Big Bind
+                </h1>
+                <div class="blindDetails">
+                    Score at least ?
+                </div>
+            </div>
             <div class="roundScore">
                 <h1 class="scoreTitle">
                     Round Score
                 </h1>
                 <h1 class="scoreValue">
-                    ?
+                    0
                 </h1>
             </div>
             <div class="handDetails">
-                <div class="handShape displayCenter">MOVE?</div>
-                <div class="multiplier displayCenter">? X ?</div>
+                <div class="handShape displayCenter">FLUSH</div>
+                <div class="multiplier displayCenter">10 x 15</div>
             </div>
             <div class="runDetails">
                 RUN DETAILS
