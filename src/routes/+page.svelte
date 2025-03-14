133242be477ce3da4b8bf79e659b30041639a398
Zenrieru <nvll.zen@gmail.com>
2025-03-14 08:16:20 +0700

    V1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import "../styles/global.css"
+</script>
+<div class="mainScreen">
+        <div class="menu">
+            <div class="roundDetails">? Bind</div>
+            <div class="roundScore">
+                <h1 class="scoreTitle">
+                    Round Score
+                </h1>
+                <h1 class="scoreValue">
+                    ?
+                </h1>
+            </div>
+            <div class="handDetails">
+                <div class="handShape displayCenter">FL?SH</div>
+                <div class="multiplier displayCenter">? X ?</div>
+            </div>
+            <div class="runDetails">
+                RUN DETAILS
+            </div>
+        </div>
+        <div class="playground">
+            <div class="consumables displayCenter">consumables</div>
+            <div class="table displayCenter">table</div>
+            <div class="hand displayCenter">hand</div>
+        </div>
+</div>
